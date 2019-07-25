a49f613138e9d0993fbfb873f8e66defb8f19ab1
brar220 <52762169+brar220@users.noreply.github.com>
2019-07-24 23:22:39 -0400

    Add files via upload

---
+++ assets/data/mySongs.ts
@@ -0,0 +1,34 @@
+import { SongInfo } from '../../app/songInfo'; 
+
+export const MYSONGS: SongInfo[]=[{
+   sname:'Sakhiyaan',
+   artist:'Maninder Butter',
+   genre:'Romantic',
+   year:'2018',
+   pic:'sakh'
+
+},
+{
+sname:'Backbone',
+artist:'Hardy Sandhu',
+genre:'Romantic',
+year:'2017',
+pic:'back'
+}
+,
+{
+sname:'Qismat',
+artist:'Ammy Virk',
+genre:'Sad',
+year:'2018',
+pic:'qis'
+},
+{
+sname:'Dabda kithe Aa',
+artist:'R Nait',
+genre:'Action',
+year:'2019',
+pic:'dabda'
+}
+
+]
